ac1263390e34e9dba47962878a59c79f29871422
Maximilian Sparenberg <maximilian.sparenberg@gmail.com>
2018-02-25 19:20:21 +0100

    editor entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,60 +1,80 @@
-#!/bin/bash
-set -e
+#!/bin/sh
 
-if [ "${1:0:1}" = '-' ]; then
-	set -- mysqld_safe "$@"
+# parameters
+MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-"mysql"}
+MYSQL_USER=${MYSQL_USER:-""}
+MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
+MYSQL_DATABASE=${MYSQL_DATABASE:-""}
+
+echo "$MYSQL_ROOT_PASSWORD"
+echo "$MYSQL_USER"
+echo "$MYSQL_PASSWORD"
+echo "$MYSQL_DATABASE"
+
+if [ ! -d "/run/mysqld" ]; then
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
 fi
 
-if [ "$1" = 'mysqld_safe' ]; then
-	DATADIR="/var/lib/mysql"
+if [ -d /var/lib/mysql/mysql ]; then
+	echo '[i] MySQL directory already present, skipping creation'
+else
+	echo "[i] MySQL data directory not found, creating initial DBs"
 
-	if [ ! -d "$DATADIR/mysql" ]; then
-		if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" ]; then
-			echo >&2 'error: database is uninitialized and MYSQL_ROOT_PASSWORD not set'
-			echo >&2 '  Did you forget to add -e MYSQL_ROOT_PASSWORD=... ?'
-			exit 1
-		fi
+	chown -R mysql:mysql /var/lib/mysql
 
-		echo 'Running mysql_install_db ...'
-		mysql_install_db --datadir="$DATADIR"
-		echo 'Finished mysql_install_db'
+	# init database
+	echo 'Initializing database'
+	mysql_install_db --user=mysql > /dev/null
+	echo 'Database initialized'
 
-		# These statements _must_ be on individual lines, and _must_ end with
-		# semicolons (no line breaks or comments are permitted).
-		# TODO proper SQL escaping on ALL the things D:
+	echo "[i] MySql root password: $MYSQL_ROOT_PASSWORD"
 
-		tempSqlFile='/tmp/mysql-first-time.sql'
-		cat > "$tempSqlFile" <<-EOSQL
-			DELETE FROM mysql.user ;
-			CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
-			GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
-			DROP DATABASE IF EXISTS test ;
-		EOSQL
-
-		if [ "$MYSQL_DATABASE" ]; then
-			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" >> "$tempSqlFile"
-			if [ "$MYSQL_CHARSET" ]; then
-				echo "ALTER DATABASE \`$MYSQL_DATABASE\` CHARACTER SET \`$MYSQL_CHARSET\` ;" >> "$tempSqlFile"
-			fi
-
-			if [ "$MYSQL_COLLATION" ]; then
-				echo "ALTER DATABASE \`$MYSQL_DATABASE\` COLLATE \`$MYSQL_COLLATION\` ;" >> "$tempSqlFile"
-			fi
-		fi
-
-		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
-			echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> "$tempSqlFile"
-
-			if [ "$MYSQL_DATABASE" ]; then
-				echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" >> "$tempSqlFile"
-			fi
-		fi
-
-		echo 'FLUSH PRIVILEGES ;' >> "$tempSqlFile"
-
-		set -- "$@" --init-file="$tempSqlFile"
+	# create temp file
+	tempSqlFile='/tmp/mysql-first-time.sql'
+	if [ ! -f "$tempSqlFile" ]; then
+	    return 1
 	fi
 
+	# save sql
+	echo "[i] Create temp file: $tempSqlFile"
+	cat << EOF > $tempSqlFile
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+EOF
+
+
+	# Create new database
+	if [ "$MYSQL_DATABASE" != "" ]; then
+		echo "[i] Creating database: $MYSQL_DATABASE"
+		echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tempSqlFile
+
+		# set new User and Password
+		if [ "$MYSQL_USER" != "" ] && [ "$MYSQL_PASSWORD" != "" ]; then
+		echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
+		echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tempSqlFile
+		fi
+	else
+		# don`t need to create new database,Set new User to control all database.
+		if [ "$MYSQL_USER" != "" ] && [ "$MYSQL_PASSWORD" != "" ]; then
+		echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
+		echo "GRANT ALL ON *.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tempSqlFile
+		fi
+	fi
+
+	echo 'FLUSH PRIVILEGES;' >> $tempSqlFile
+
+	# run sql in tempfile
+	echo "[i] run tempfile: $tempSqlFile"
+	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < $tempSqlFile
+	#rm -f $tempSqlFile
 fi
 
-exec "$@"
+echo "[i] Sleeping 5 sec"
+sleep 5
+
+echo '[i] start running mysqld'
+exec /usr/bin/mysqld --user=mysql --console
